c42ec395e9f176ec2d5460b15eed6eae345fbd7a
Jhonattan Millan <millanjhonattan0@gmail.com>
2020-05-12 18:40:17 -0400

    Fix friends posts test

--- social/tests/test_view_home.py
+++ social/tests/test_view_home.py
@@ -19,7 +19,7 @@ class HomeViewTests(TestCase):
 		self.assertEquals(view.func.view_class, HomeView)
 
 	def test_contains_search_bar(self):
-		self.assertContains(self.response, '<input class="form-control" type="text" id="namefield" placeholder="Search people">')
+		self.assertContains(self.response, '<input id="namefield"')
 
 class FeedPostsTests(TestCase):
 	def setUp(self):
@@ -27,6 +27,10 @@ class FeedPostsTests(TestCase):
 		self.friend = User.objects.create_user(username='badcc', first_name='Bad', last_name='Badimo',email='rblx@outlook.com', password='contraseña98989')
 		random = User.objects.create_user(username='brandon_b', first_name='Obscured', last_name='Entity', email='booga@booga.com', password='thefireisland')
 		
+		Profile.objects.create(user=user)
+		Profile.objects.create(user=self.friend)
+		Profile.objects.create(user=random)
+
 		self.friend_post = Post.objects.create(message='Lorem ipsum dolor sit amet.', created_by=self.friend.profile)
 		Post.objects.create(message='Optio totam animi numquam facere ratione iure.', created_by=random.profile)
 		Friendship.objects.create(creator=self.friend.profile, friend=user.profile)
